1382731c889eef102d5c183067d0debe04d30c85
LucasRosas <lucasrosas@outlook.com>
2024-05-30 18:01:44 -0300

    finalização do game

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
     
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent){
@@ -15,24 +15,69 @@ struct MemoryGame<CardContent> {
         // add numberOfPairsOfCards x 2 cards
         for pairIndex in 0..<max(2, numberOfPairsOfCards) {
             let content = cardContentFactory(pairIndex)
-            cards.append(Card( content: content))
-            cards.append(Card( content: content))
-
+            cards.append(Card(content: content, id: "\(pairIndex)a"))
+            cards.append(Card(content: content, id: "\(pairIndex)b"))
         }
+        shuffle()
     }
     
-    func choose(card: Card) {
-        
+    var firstIndex: Int?
+    var secondIndex: Int?
+    
+     mutating func choose(card: Card) {
+         if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }){
+             if !cards[chosenIndex].isMatched && !cards[chosenIndex].isFaceUp {
+                 cards[chosenIndex].isFaceUp = true
+                 if let f = firstIndex {
+                     if let s = secondIndex {
+                         
+                         if(cards[f].content == cards[s].content && f != s){
+                             cards[f].isMatched = true
+                             cards[s].isMatched = true
+                         } else {
+                             
+                             cards[f].isFaceUp = false
+                             cards[s].isFaceUp = false
+                         }
+                         firstIndex = chosenIndex
+                         secondIndex = nil
+                         
+                     } else {
+                         secondIndex = chosenIndex
+                         
+                     }
+                 } else {
+                     firstIndex = chosenIndex
+                 }
+             }
+         }
+    }
+    
+    private func index(of card: Card) -> Int? {
+        for index in cards.indices {
+            if cards[index].id == card.id{
+                return index
+            }
+        }
+        return nil
     }
     
     mutating func shuffle(){
         cards.shuffle()
     }
     
-    struct Card {
-        var isFaceUp = true
+    struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
+        
+        
+        
+        var isFaceUp = false
         var isMatched = false
         let content: CardContent
+        
+        var id: String
+        var debugDescription: String {
+            "\(content)"
+        }
     }
     
 }
