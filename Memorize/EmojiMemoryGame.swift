feba3470858511b05d0581dac0a2d3cd850da699
LucasRosas <lucasrosas@outlook.com>
2024-05-30 12:58:39 -0300

    que isso

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -5,4 +5,36 @@
 //  Created by Lucas Araújo Rosas on 30/05/24.
 //
 
-import Foundation
+import SwiftUI
+
+
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["🥎", "💛", "🔔",  "😡", "🦞", "🐝" ]
+
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame( numberOfPairsOfCards: 8) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            }else{
+                return "🐙"
+            }
+        }
+    }
+    
+    
+    @Published private var model = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+    
+    // MARK:  Intents
+    func shuffle(){
+        model.shuffle()
+    }
+}
+
